786fee86f90b75723d939695db51773dcab88651
CodeFather-Saeed <kazisaeed8056@gmail.com>
2023-10-14 05:36:17 +0600

    Basic Calculator

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-# This is a sample Python script.
+a = 60
+b = 40
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+print("First Number is = ",a)
+print("Second Number is = ",b)
+print("\n")
 
+sum = a + b
+sub = a - b
+mul = a * b
+div = a / b
+mod = a % b
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+print("Summation is = ",sum)
+print("Subtraction is = ",sub)
+print("Multiplier is = ",mul)
+print("Division is = ",div)
+print("Modulus is = ",mod)
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
