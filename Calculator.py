833dba46b4177a8ba7683f8eb45c2f34e9c14d41
CodeFather-Saeed <kazisaeed8056@gmail.com>
2023-10-15 02:34:55 +0600

    Calculator update 1.001 version

--- Calculator.py
+++ Calculator.py
@@ -1,8 +1,9 @@
-a = int(input())   #In one line two input is a, b = input().split()
-b = int(input())
+a = int(input('Enter your first Number = '))  #In one line two input is a, b = input().split()
+b = int(input('Enter your second Number = '))
+print("\n")
 
-print("First Number is = ",a)
-print("Second Number is = ",b)
+print("First Number Enter  = ",a)
+print("Second Number Enter = ",b)
 print("\n")
 
 sum = a + b
